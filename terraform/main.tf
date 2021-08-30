2cd44ebb8c9f2f6c5b24ab5b794a2a47d76a5866
urbeingwatched8 <tupikina_natasha@mail.ru>
2021-08-30 14:05:16 +0300

    lab 3 working with notes

---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+provider "virtualbox" {
+  # Configuration options
+}
+
+resource "virtualbox_vm" "node" {
+  count     = 2
+  name      = format("node-%02d", count.index + 1)
+  image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
+  cpus      = 2
+  memory    = "512 mib"
+  user_data = file("${path.module}/user_data")
+
+  network_adapter {
+    type           = "hostonly"
+    host_interface = "vboxnet1"
+  }
+}
